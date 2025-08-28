170ec25d83c5df52e35cc53c9ca249b6b2770799
yaskho <yassineholberton@gmail.com>
2025-08-28 20:11:30 +0100

    bhbhk

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,29 +1,9 @@
-Create a function named groceriesList that returns a map of groceries with the following items (name, quantity):
-
-Apples, 10
-Tomatoes, 10
-Pasta, 1
-Rice, 1
-Banana, 5
-Result:
-
-bob@dylan:~$ cat 9-main.js
-import groceriesList from "./9-groceries_list.js";
-
-console.log(groceriesList());
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 9-main.js 
-Map {
-  'Apples' => 10,
-  'Tomatoes' => 10,
-  'Pasta' => 1,
-  'Rice' => 1,
-  'Banana' => 5
+export default function groceriesList() {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 5);
+  map.set('Rice', 5);
+  map.set('Bananas', 7);
+  return map;
 }
-bob@dylan:~$ 
-Repo:
-
-GitHub repository: holbertonschool-web_back_end
-Directory: ES6_data_manipulation
-File: 9-groceries_list.js
