1d08cc151b90956abd24fa1e31331551a0e3ec8a
yaskho <yassineholberton@gmail.com>
2025-11-25 11:06:01 +0100

    hvbhb

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,9 +2,8 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(photo.body, user.firstName, user.lastName);
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
