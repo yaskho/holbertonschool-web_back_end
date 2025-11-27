8eeb85a37ea6ca23b63b620de31aa23b6f83d0cc
yaskho <yassineholberton@gmail.com>
2025-11-27 12:46:49 +0100

    hbhvggg

---
+++ python_async_function/0-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+print(asyncio.run(wait_random()))
+print(asyncio.run(wait_random(5)))
+print(asyncio.run(wait_random(15)))
+
+bob@dylan:~$ ./0-main.py
+9.034261504534394
+1.6216525464615306
+10.634589756751769
